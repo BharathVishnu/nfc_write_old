a8de4879494d06a750dfad5a2986f5de291880fc
Bharath Vishnu B <bharathvb1204@gmail.com>
2024-04-07 23:56:21 +0530

    old app

--- android/app/src/main/kotlin/com/example/flutter_kotlin_integration/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_kotlin_integration/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.example.flutter_kotlin_integration
 
 import android.content.Intent
-import android.nfc.NdefMessage
-import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
-import android.nfc.Tag
-import android.nfc.tech.Ndef
 import android.os.Bundle
 import android.widget.Toast
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import java.nio.charset.Charset
 import com.example.flutter_kotlin_integration.KHostApduService
+
 
 class MainActivity : FlutterActivity() {
     private var mNfcAdapter: NfcAdapter? = null
@@ -37,40 +34,22 @@ class MainActivity : FlutterActivity() {
         }
     }
 
-    private fun startNFCService(ndefMessageText: String) {
+    private fun startNFCService(ndefMessage: String) {
         if (checkNFCEnable()) {
-            // Create NDEF record
-            val ndefRecord = createTextRecord(ndefMessageText)
-
-            // Encode NDEF message
-            val ndefMessage = NdefMessage(ndefRecord)
-
-            // Start NFC transmission
-            val intent = Intent(this, javaClass).apply {
-                action = NfcAdapter.ACTION_TAG_DISCOVERED
-                putExtra(NfcAdapter.EXTRA_NDEF_MESSAGES, arrayOf(ndefMessage))
-            }
-            startActivity(intent)
+            val intent = Intent(this, KHostApduService::class.java) // Replace KHostApduService with the correct class name
+            intent.putExtra("ndefMessage", ndefMessage)
+            startService(intent)
         } else {
             Toast.makeText(this, "NFC is not enabled", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun stopNFCService() {
-        // No action needed here, as NFC transmission is stopped automatically
+        val intent = Intent(this, KHostApduService::class.java) // Replace KHostApduService with the correct class name
+        stopService(intent)
     }
 
     private fun checkNFCEnable(): Boolean {
         return mNfcAdapter?.isEnabled == true
-    }
-
-    private fun createTextRecord(text: String): NdefRecord {
-        val languageCode: ByteArray = "en".toByteArray(Charset.forName("US-ASCII"))
-        val textBytes: ByteArray = text.toByteArray(Charset.forName("UTF-8"))
-        val payload = ByteArray(textBytes.size + 1)
-        payload[0] = 0x02.toByte() // UTF-8
-        System.arraycopy(languageCode, 0, payload, 1, languageCode.size)
-        System.arraycopy(textBytes, 0, payload, languageCode.size + 1, textBytes.size)
-        return NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, byteArrayOf(), payload)
     }
 }
